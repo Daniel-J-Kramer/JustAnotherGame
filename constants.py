9be206fbb0d913ac0247e0405ca470f6a40c70f2
Daniel-J-Kramer <daniel.kramer.j@gmail.com>
2026-08-07 19:56:47 -0500

    Moved constants and started adding logic to game, now supports keypresses

--- constants.py
+++ constants.py
@@ -6,9 +6,18 @@ SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 360
 TEXTBOX_SIZE = (int(SCREEN_WIDTH//10), int(SCREEN_HEIGHT//1.2))
 
+#Text constants
+FONT = pygame.font.Font('freesansbold.ttf', 16)
+TEXTCOLOR = (255,255,255)
+TEXTSTRING = "Want to play a game???"
+SLOWTEXT = .2
+NORMAL = .15
+FASTTEXT = .075
+TEXTSPEED = NORMAL
+
 #game constants
-choice = 0
-path = 0
-savestate = 0
-modifier = 0
-delay = .03
+CHOICE = 0
+PATH = 0
+SAVESTATE = 0
+MODIFIER = 0
+DELAY = .03
--- main.py
+++ main.py
@@ -15,23 +15,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("GreyTone")
     pygame.font.init()
-    font = pygame.font.Font('freesansbold.ttf', 16)
-    textColor = (255,255,255)
-    text = font.render("", True, textColor)
-    textstring = "Want to play a game???"
-    slowtext = .2
-    normal = .15
-    fast = .075
-    textspeed = normal
+    text = FONT.render("", True, constants.TEXTCOLOR)
     textlength = 0
     textBackground = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT//1.2))
     textBackground.fill((0,0,255))
     textBox = text.get_rect()
     textBox.center = TEXTBOX_SIZE
-    block = False
-    display = True
     clock = pygame.time.Clock()
     dt = 0
+    block = False
+    display = True
     #Insert Groups
     #End Insert Groups
     #Insert Containers
@@ -46,17 +39,32 @@ def main():
            if event.type == pygame.QUIT:
                return
     #Begin Game Math Code
-        if textlength == len(textstring) + 1:
+        if textlength == len(TEXTSTRING) + 1:
             if block == False:
                 display = not display
                 block = True
         else:    
             block = False
         if display:
-            text, textlength = textWriter(textstring, font, textlength, textColor)
-            time.sleep(textspeed)
+            text, textlength = textWriter(TEXTSTRING, FONT, textlength, constants.TEXTCOLOR)
+            time.sleep(TEXTSPEED)
         elif not display:
-            pass
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_1]:
+                text, textlength = FONT.render("", True, TEXTCOLOR), 0
+                display = True
+            if keys[pygame.K_2]:
+                constants.TEXTCOLOR = (255,0,0)
+                text, textlength = FONT.render("", True, TEXTCOLOR), 0
+                display = True
+            if keys[pygame.K_3]:
+                constants.TEXTCOLOR = (255,255,255)
+                text, textlength = FONT.render("", True, TEXTCOLOR), 0
+                display = True
+            if keys[pygame.K_4]:
+                constants.TEXTCOLOR = (0,255,0)
+                text, textlength = FONT.render("", True, TEXTCOLOR), 0
+                display = True
     #End Game Math Code
 #End Game Code
 #-------------------------------------------------------------------
